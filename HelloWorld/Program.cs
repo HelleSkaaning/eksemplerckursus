f43806c89953ed0bc98a5a296dd862ed2258a7ea
Helle <hsk@itrelation.dk>
2017-08-29 14:48:00 +0200

    Helloworld program initially

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string navn = "";
+
+            Console.WriteLine("Indtast navn");
+            navn = Console.ReadLine();
+            navn = navn.ToUpper();
+
+            Console.WriteLine("Velkommen "+navn.ToLower());
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Tryk på en tast for at fortsætte . . . ");
+                System.Console.ReadKey();
+            }
+
+        }
+    }
+}
