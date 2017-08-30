aaaba1272da242ab68f4da300244e30ca4b2a8c0
Helle <hsk@itrelation.dk>
2017-08-30 14:57:37 +0200

    dodit

--- Module04_ReferenceTypes/Program.cs
+++ Module04_ReferenceTypes/Program.cs
@@ -1,111 +1,134 @@
-﻿namespace Module04_ReferenceTypes
+﻿using
+    System;
+
+namespace Module04_ReferenceTypes
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            if (false)
             {
 
-            PersonStruct p1;
-            //p1.Id++;
-            p1.Id = 2;
-            p1.ErILive = true;
 
-            PersonStruct p2 = new PersonStruct(); // Brug det der new for at sikre at der er intialiseret
-            p2.Id++;
-            p2.Id = 2;
-            p2.ErILive = true;
-
-            System.Console.WriteLine(p1.Id);
-            System.Console.WriteLine(p2.Id);
-
-            p1 = p2; // kopierer værdier IKKE reference
-            p2.Id = 5;
-
-            System.Console.WriteLine(p1.Id);
-            System.Console.WriteLine(p2.Id);
-
-            // De fleste vil lave en klassse ikke en struct
-            // Der hvor man gerne vil kopiere værdier er det bedre at bruge structs
-            // struct er en mindre sammensat type
-
-            System.Console.WriteLine();
-
-            PersonClass p3;
-            p3 = new PersonClass();
-            p3.Id = 3;
-            p3.ErILive = true;
-            //System.Console.WriteLine(p3.Id);
-
-            PersonClass p4 = new PersonClass();
-            p4.Id = 4;
-            p4.ErILive = false;
-
-            System.Console.WriteLine(p3.Id);
-            System.Console.WriteLine(p4.Id);
-
-            p3 = p4; // så peger p3 reference nu på samme memory som p4
-            System.Console.WriteLine(p3.Id);
-            System.Console.WriteLine(p4.Id);
-            }
-
-            {
-
-                System.Console.WriteLine();
-                System.Console.WriteLine();
-
-                int[] i;
-                i = new int[5];
-
-                int[] x = new int[5];
-
-                x[0] = 2;
-                //x[0] = "dsfdsf"; NEJ!!!
-                x[1] = 3;
-                x[3] = 76;
-                //x[8] = 22; NEJ!!!!!
-
-                System.Console.WriteLine(x[0]);
-
-                System.Console.WriteLine(x);
-
-                for (int r = 0; r < x.Length; r++)
                 {
-                    System.Console.WriteLine(x[r]);
+
+                    PersonStruct p1;
+                    //p1.Id++;
+                    p1.Id = 2;
+                    p1.ErILive = true;
+
+                    PersonStruct p2 = new PersonStruct(); // Brug det der new for at sikre at der er intialiseret
+                    p2.Id++;
+                    p2.Id = 2;
+                    p2.ErILive = true;
+
+                    System.Console.WriteLine(p1.Id);
+                    System.Console.WriteLine(p2.Id);
+
+                    p1 = p2; // kopierer værdier IKKE reference
+                    p2.Id = 5;
+
+                    System.Console.WriteLine(p1.Id);
+                    System.Console.WriteLine(p2.Id);
+
+                    // De fleste vil lave en klassse ikke en struct
+                    // Der hvor man gerne vil kopiere værdier er det bedre at bruge structs
+                    // struct er en mindre sammensat type
+
+                    System.Console.WriteLine();
+
+                    PersonClass p3;
+                    p3 = new PersonClass();
+                    p3.Id = 3;
+                    p3.ErILive = true;
+                    //System.Console.WriteLine(p3.Id);
+
+                    PersonClass p4 = new PersonClass();
+                    p4.Id = 4;
+                    p4.ErILive = false;
+
+                    System.Console.WriteLine(p3.Id);
+                    System.Console.WriteLine(p4.Id);
+
+                    p3 = p4; // så peger p3 reference nu på samme memory som p4
+                    System.Console.WriteLine(p3.Id);
+                    System.Console.WriteLine(p4.Id);
                 }
 
-                System.Console.WriteLine();
-                System.Console.WriteLine();
+                {
 
-                int[] xx = { 2, 5, 0, 76, 0};
+                    System.Console.WriteLine();
+                    System.Console.WriteLine();
+
+                    int[] i;
+                    i = new int[5];
+
+                    int[] x = new int[5];
+
+                    x[0] = 2;
+                    //x[0] = "dsfdsf"; NEJ!!!
+                    x[1] = 3;
+                    x[3] = 76;
+                    //x[8] = 22; NEJ!!!!!
+
+                    System.Console.WriteLine(x[0]);
+
+                    System.Console.WriteLine(x);
+
+                    for (int r = 0; r < x.Length; r++)
+                    {
+                        System.Console.WriteLine(x[r]);
+                    }
+
+                    System.Console.WriteLine();
+                    System.Console.WriteLine();
+
+                    int[] xx = { 2, 5, 0, 76, 0 };
+
+
+                }
+
+
+                {
+                    int[] j = { 5, 3, 74 };
+                    int[] x = { 7, 8, 1, 6, 5, 4 };
+
+                    System.Console.WriteLine(j[1]);
+                    System.Console.WriteLine(x[1]);
+                    System.Console.WriteLine(j == x);
+                    j = x; // Nu er de ens
+
+                    System.Console.WriteLine(j[1]);
+                    System.Console.WriteLine(x[1]);
+                    System.Console.WriteLine(j == x);
+
+
+                    System.Array.Sort(x);
+
+                    System.Console.WriteLine();
+                    System.Console.WriteLine(x.Length);
+                }
 
 
             }
 
+            int y = 0;
+            bool f = false;
 
+            if (y> 0 && f == true)
             {
-                int[] j = {5,3,74 };
-                int[] x = { 7, 8, 1,6, 5, 4 };
 
-                System.Console.WriteLine(j[1]);
-                System.Console.WriteLine(x[1]);
-                System.Console.WriteLine(j==x);
-                j = x; // Nu er de ens
-
-                System.Console.WriteLine(j[1]);
-                System.Console.WriteLine(x[1]);
-                System.Console.WriteLine(j == x);
-
-
-                System.Array.Sort(x);
-
-                System.Console.WriteLine();
-                System.Console.WriteLine(x.Length);
+            }
+            else
+            {
+ 
             }
 
+            
+            
 
-
+            
 
 
             if (System.Diagnostics.Debugger.IsAttached)
---
+++ methods/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            string outStr = "";
+            for (int i = 1; i <= 10; i++)
+            {
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    sum = j * i;
+                    outStr = sum.ToString();
+                    //outStr = (j * i).ToString();
+                    if (sum < 50)
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.White;
+                        System.Console.BackgroundColor = ConsoleColor.Red;
+                    }
+                    else
+                    {
+                        System.Console.ForegroundColor = ConsoleColor.Yellow;
+                        System.Console.BackgroundColor = ConsoleColor.Blue;
+                    }
+
+
+                    Console.Write(outStr.PadLeft(4));
+
+                }
+                Console.WriteLine();
+            }
+
+            System.Console.ForegroundColor = ConsoleColor.White;
+            System.Console.BackgroundColor = ConsoleColor.Black;
+            Console.WriteLine();
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
