a1714032c295aab17614e8b582866871382dfda2
Helle <hsk@itrelation.dk>
2017-08-30 14:00:30 +0200

    Saved

---
+++ Arrays/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int[] salary = { 10000, 20000, 15000, 20000, 30000, 15000 };
+                double total = 0;
+
+                for (int i = 0; i < salary.Length; i++)
+                {
+                    Console.WriteLine("Månedsløn: " + salary[i].ToString("C2"));
+                    total += salary[i];
+                }
+
+                Console.WriteLine("Total: " + total.ToString("C2"));
+                double gnsnt = total / salary.Length;
+                Console.WriteLine("Gnsnt: " + gnsnt.ToString("C2"));
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                System.Array.Sort(salary);
+
+                for (int i = 0; i < salary.Length; i++)
+                {
+                    Console.WriteLine(salary[i].ToString("C2"));
+                }
+
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+                // hurtigere udgave:
+                Console.WriteLine(salary.Sum());
+                Console.WriteLine(salary.Average());
+                string res = string.Join(",", salary);
+                Console.WriteLine(res);
+
+            }
+
+
+            {
+                string u = null;
+
+                u = "Mikkel";
+                Console.WriteLine(u.ToString());
+                Console.WriteLine(u);
+
+                string w = u + " \t\t***\r\n "+u;
+
+                Console.WriteLine(w);
+
+                string stiTilFil = @"c:\temp\trunte.txt";
+                Console.WriteLine(stiTilFil);
+
+                stiTilFil = "c:\\temp\\trunte.txt";
+                Console.WriteLine(stiTilFil);
+
+                Console.WriteLine(u.ToUpper());
+
+                Console.WriteLine(u.Substring(2,3));
+
+                Console.WriteLine();
+                Console.WriteLine();
+
+
+            }
+
+
+            {
+
+                string a = "";
+                Console.WriteLine("Start");
+                for (int i = 0; i < 1_000_000; i++)
+                {
+//                    a = a + '*'; // NEJ 
+
+                }
+                Console.WriteLine("Slut");
+
+                System.Text.StringBuilder sb = new StringBuilder();
+                Console.WriteLine("Start");
+                for (int i = 0; i < 1_000_000; i++)
+                {
+                    sb.Append("*");
+                }
+                Console.WriteLine("Slut");
+
+
+            }
+
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+
+            string samletNavn = fornavn + ' ' + efternavn;
+            Console.WriteLine(samletNavn);
+            string navnStort = samletNavn.ToUpper();
+            Console.WriteLine(navnStort);
+            string navnlille = samletNavn.ToLower();
+            Console.WriteLine(navnlille);
+            string navnDel = samletNavn.Substring(fornavn.Length+1,4);
+            Console.WriteLine(navnDel);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            string[] split = samletNavn.Split(' ');
+            for (int i = 0; i < split.Length; i++)
+            {
+                Console.WriteLine("Split["+i.ToString()+"]: "+split[i]);
+            }
+
+            //System.IO.File.WriteAllText(@"c:\temp\helle.txt", samletNavn);
+            System.IO.File.WriteAllText("c:\\temp\\helle.txt", samletNavn);
+            Console.WriteLine("Fil oprettet og tilskrevet");
+
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                System.Console.Write("Press any key to continue . . . ");
+                System.Console.ReadKey();
+            }
+        }
+    }
+}
